e0465491000de995149dff04c973e59107cfd4ed
Maribelle Leong <maribelle.leong@email.com>
2020-07-06 18:16:20 -0300

    added sub components empty files for Appoinment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss';
+
+const Appointment = (props) => {
+  return <article className='appointment'></article>;
+};
+
+Appoinment.propTypes = {};
+
+export default Appointment;
