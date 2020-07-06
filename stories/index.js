dd38fc602a908d37eccc2f3abfac60de8aac4195
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-06 20:01:37 -0300

    added stories for Show component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerListItem from 'components/InterviewerListItem';
 import Appointment from 'components/Appointment';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
 
 storiesOf('Button', module)
   .addParameters({
@@ -138,4 +139,12 @@ storiesOf('Appoinment', module)
   .add('Appointment', () => <Appointment />)
   .add('Appointment with Time', () => <Appointment time='12pm' />)
   .add('Header', () => <Header time='12pm' />)
-  .add('Empty', () => <Empty onAdd={action('onAdd')} />);
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => (
+    <Show
+      student='Lydia Miller-Jones'
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ));
