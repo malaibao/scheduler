e9450b87e96af0e63d93be3b28132f8e8b0338bd
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-06 15:27:21 -0300

    mapped props.days on DayListItem

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import DayListItem from './DayListItem';
 import PropTypes from 'prop-types';
 
 function DayList(props) {
-  return <ul></ul>;
+  return (
+    <ul>
+      {props.days.map((day) => (
+        <DayListItem
+          key={day.id}
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.day}
+          setDay={props.setDay}
+        />
+      ))}
+    </ul>
+  );
 }
 
 DayList.propTypes = {};
