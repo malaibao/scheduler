eea7e21f284d9d46e562a823095fa0619b8607a6
Maribelle Leong <maribelle.leong@email.com>
2020-07-07 15:41:15 -0300

    imported Show and Empty components to Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
@@ -7,6 +9,14 @@ const Appointment = (props) => {
   return (
     <article className='appointment'>
       <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
     </article>
   );
 };
