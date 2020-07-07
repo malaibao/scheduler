3e1b7b5e33c4cdb54ec644d39318e56675a57c34
Maribelle Leong <maribelle.leong@email.com>
2020-07-07 16:10:38 -0300

    added Appointment component to it and some fake data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import DayList from './DayList';
+import Appointment from './Appointment';
 import 'components/Application.scss';
 
 const days = [
@@ -17,6 +18,58 @@ const days = [
     id: 3,
     name: 'Wednesday',
     spots: 0,
+  },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: '12pm',
+    interview: {
+      student: 'John Doe',
+      interviewer: {
+        id: 1,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  {
+    id: 2,
+    time: '1pm',
+  },
+  {
+    id: 3,
+    time: '2pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 1,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+
+  {
+    id: 4,
+    time: '3pm',
+    interview: {
+      student: 'Mary Doe',
+      interviewer: {
+        id: 1,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  {
+    id: 5,
+    time: '4pm',
+  },
+  {
+    id: 6,
+    time: '5pm',
   },
 ];
 
@@ -42,7 +95,16 @@ export default function Application(props) {
         />
       </section>
       <section className='schedule'>
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((appointment) => (
+          <Appointment
+            key={
+              appointment.id === appointments.length - 1
+                ? 'last'
+                : appointments.id
+            }
+            {...appointment}
+          />
+        ))}
       </section>
     </main>
   );
