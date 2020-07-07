223fe34b883dccf2d6060c407b461793783cb5f8
Maribelle Leong <maribelle.leong@email.com>
2020-07-07 13:33:10 -0300

    created Error component

---
+++ src/components/Appointment/Error.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Error = (props) => {
+  return (
+    <main className='appointment__card appointment__card--error'>
+      <section className='appointment__error-message'>
+        <h1 className='text--semi-bold'>Error</h1>
+        <h3 className='text--light'>{props.message}</h3>
+      </section>
+      <img
+        className='appointment__error-close'
+        src='images/close.png'
+        alt='Close'
+        onClick={props.onClose}
+      />
+    </main>
+  );
+};
+
+Error.propTypes = {
+  message: PropTypes.string,
+  onClose: PropTypes.func,
+};
+
+export default Error;
