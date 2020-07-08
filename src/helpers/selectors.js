5cf935d86900554913fd9ebc9e1c441f4599d64a
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-08 15:47:39 -0300

    removed dump for loop for getting appointments

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,26 +1,29 @@
-export const getAppointmentsForDay = (state, day) => {
-  const result = [];
-  const stateDays = [...state.days];
-  const stateAppointments = { ...state.appointments };
+/* example of result
+[
+  { id: 1, time: '12pm', interview: null },
+  { id: 2, time: '1pm', interview: null },
+  {
+    id: 3,
+    time: '2pm',
+    interview: { student: 'Archie Cohen', interviewer: 2 },
+  },
+]
+*/
 
-  // check if days data is empty
-  if (stateDays.length < 1) {
+export const getAppointmentsForDay = (state, day) => {
+  // check if days data is empty,
+  if (state.days.length < 1) {
     return [];
   }
 
   // Find day in state
-  const foundDay = stateDays.find((stateDay) => stateDay.name === day);
+  const foundDay = state.days.find((stateDay) => stateDay.name === day);
 
   // check if day not found
   if (!foundDay) {
     return [];
   }
 
-  for (let [key, value] of Object.entries(stateAppointments)) {
-    if (foundDay.appointments.includes(Number(key))) {
-      result.push(value);
-    }
-  }
-
+  const result = foundDay.appointments.map((id) => state.appointments[id]);
   return result;
 };
