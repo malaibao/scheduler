2a2caa913e86fac20a073a28727f378709fc68f5
Maribelle Leong <maribelle.leong@email.com>
2020-07-09 15:32:20 -0300

    created getInterviewersForDay function and test cases for it

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -27,6 +27,24 @@ export const getAppointmentsForDay = (state, day) => {
   return result;
 };
 
+export const getInterviewersForDay = (state, day) => {
+  // check if days data is empty,
+  if (state.days.length < 1) {
+    return [];
+  }
+
+  // Find day in state
+  const foundDay = state.days.find((stateDay) => stateDay.name === day);
+
+  // check if day not found
+  if (!foundDay) {
+    return [];
+  }
+
+  const result = foundDay.interviewers.map((id) => state.interviewers[id]);
+  return result;
+};
+
 /*
 {  
   "student": "Lydia Miller-Jones",
