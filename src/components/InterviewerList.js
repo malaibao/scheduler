1f00bd7a3db342fb7bdd31d94b27a3cbd60a8570
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-09 15:00:56 -0300

    added && in mapping

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -8,15 +8,16 @@ function InterviewerList(props) {
     <section className='interviewers'>
       <h4 className='interviewers__header text--light'>Interviewer</h4>
       <ul className='interviewers__list'>
-        {props.interviewers.map((interviewer) => (
-          <InterviewerListItem
-            key={interviewer.id}
-            name={interviewer.name}
-            avatar={interviewer.avatar}
-            selected={interviewer.id === props.value}
-            setInterviewer={(event) => props.onChange(interviewer.id)}
-          />
-        ))}
+        {props.interviewers &&
+          props.interviewers.map((interviewer) => (
+            <InterviewerListItem
+              key={interviewer.id}
+              name={interviewer.name}
+              avatar={interviewer.avatar}
+              selected={interviewer.id === props.value}
+              setInterviewer={(event) => props.onChange(interviewer.id)}
+            />
+          ))}
       </ul>
     </section>
   );
