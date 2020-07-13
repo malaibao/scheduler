a68234b35ccc8587c787dc009a4fbf4980dc38c3
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-13 15:15:34 -0300

    simple Jest test for Appointment

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render, cleanup } from '@testing-library/react';
+
+import Appointment from 'components/Appointment';
+
+afterEach(cleanup);
+
+describe('Appoinment', () => {
+  it('renders without crashing', () => {
+    render(<Appointment time='5pm' />);
+  });
+});
