e6a33efb8b76bb1c96853dc855eff93adab02d32
malaibao <maribelle.leong@gmail.com>
2020-07-16 12:13:17 -0300

    moved appointments test to its single file

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -11,28 +11,4 @@ describe('Navigation', () => {
       .click()
       .should('have.class', 'day-list__item--selected');
   });
-
-  it('should book an interview', () => {
-    // Clicks on the "Add" button in the second appointment
-    // Enters their name
-    // Chooses an interviewer
-    // Clicks the save button
-    // Sees the booked appointment
-    cy.get('[data-testid=appointment]')
-      .second()
-      //   .should('have.class', 'appointment__add-button')
-      .get('.appointment__add-button')
-      .click();
-
-    cy.get('[data-testid=student-name-input]').type('Einstein');
-    cy.get('li img').first().click();
-
-    cy.get('button.button--confirm').click();
-
-    cy.get(':nth-child(2) > ').should('have.text', 'Einstein');
-  });
-
-  xit('should edit an interview', () => {});
-
-  xit('should cancel an interview', () => {});
 });
