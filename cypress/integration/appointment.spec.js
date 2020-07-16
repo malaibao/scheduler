17fbc5e5bad6fc4360b7ccea0e78a03e8cb3801c
malaibao <maribelle.leong@gmail.com>
2020-07-16 12:13:04 -0300

    added integration test for booking, editing and caneling an interview

--- cypress/integration/appointment.spec.js
+++ cypress/integration/appointment.spec.js
@@ -1,27 +1,44 @@
-const { describe } = require('mocha');
-
 describe('Appointment', () => {
-  it('should book an interview', () => {
-    // Clicks on the "Add" button in the second appointment
-    // Enters their name
-    // Chooses an interviewer
-    // Clicks the save button
-    // Sees the booked appointment
-    cy.get('[data-testid=appointment]')
-      .second()
-      //   .should('have.class', 'appointment__add-button')
-      .get('.appointment__add-button')
-      .click();
-
-    cy.get('[data-testid=student-name-input]').type('Einstein');
-    cy.get('li img').first().click();
-
-    cy.get('button.button--confirm').click();
-
-    // cy.get(':nth-child(2) > ').should('have.text', 'Einstein');
+  beforeEach(() => {
+    cy.request('GET', '/api/debug/reset');
+    cy.visit('/');
+    cy.contains('Monday');
   });
 
-  xit('should edit an interview', () => {});
+  it('should book an interview', () => {
+    cy.get('[alt=Add]').first().click();
 
-  xit('should cancel an interview', () => {});
+    cy.get('[data-testid=student-name-input]').type('Einstein Jones');
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains('Save').click();
+
+    cy.contains('.appointment__card--show', 'Einstein Jones');
+    cy.contains('.appointment__card--show', 'Sylvia Palmer');
+  });
+
+  it('should edit an interview', () => {
+    cy.get('.appointment__card--show').first();
+
+    cy.get("[alt='Edit']").click({ force: true });
+
+    cy.get('[data-testid=student-name-input]').clear().type('John Doe');
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains('Save').click();
+
+    cy.contains('.appointment__card--show', 'John Doe');
+    cy.contains('.appointment__card--show', 'Tori Malcolm');
+  });
+
+  it('should cancel an interview', () => {
+    cy.get("[alt='Delete']").click({ force: true });
+    cy.contains('Confirm').click();
+
+    // Showing Deleting status spinner
+    cy.contains('Deleting');
+    cy.contains('Deleting').should('not.exist');
+
+    cy.contains('.appointment__card--show', 'Archie Cohen').should('not.exist');
+  });
 });
